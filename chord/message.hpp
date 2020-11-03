16100dd01930caccec96e70956ba115d43e3925a
mu001999 <mu001999@outlook.com>
2020-11-03 10:48:23 +0800

    update message

--- chord/message.hpp
+++ chord/message.hpp
@@ -2,15 +2,20 @@
 #define __CHORD_MESSAGE_HPP__
 
 #include <string>
+#include <vector>
+#include <optional>
 
 namespace chord
 {
+/**
+ * for messages, they are like `type,p1,p2,...,pn`
+*/
 class Message
 {
   public:
     enum Type
     {
-        Join,
+        Join, // join dst_ip:port src_ip:port
 
         Get,
         Put,
@@ -20,46 +25,77 @@ class Message
         */
         SetPredecessor,
         SetSuccessor,
-
-        Wrong,
     };
 
-    Message(const std::string &message)
+    static std::optional<Message> parse(const std::string &message)
     {
-        std::size_t pos = message.find(' ');
-        method_ = message.substr(0, pos);
-        value_ = message.substr(pos + 1);
+        std::size_t pos = message.find(',');
 
-        if (method_ == "join")
+        Type type;
+        auto method = message.substr(0, pos);
+        std::vector<std::string> params;
+
+        if (method == "join")
         {
-            type_ = Join;
+            type = Join;
         }
-        else if (method_ == "get")
+        else if (method == "get")
         {
-            type_ = Get;
+            type = Get;
         }
-        else if (method_ == "put")
+        else if (method == "put")
         {
-            type_ = Put;
+            type = Put;
         }
-        else if (method_ == "set-p")
+        else if (method == "set-p")
         {
-            type_ = SetPredecessor;
+            type = SetPredecessor;
         }
-        else if (method_ == "set-s")
+        else if (method == "set-s")
         {
-            type_ = SetSuccessor;
+            type = SetSuccessor;
         }
         else
         {
-            type_ = Wrong;
+            return {};
         }
+
+        while (pos != message.npos)
+        {
+            auto next_pos = message.find(',', pos + 1);
+            params.push_back(message.substr(pos + 1, next_pos - pos - 1));
+            pos = next_pos;
+        }
+
+        return Message(type, std::move(method), std::move(params));
     }
+
+    Message(Type type, std::string method, std::vector<std::string> params)
+      : type_(type), method_(std::move(method)), params_(std::move(params))
+    {
+        // ...
+    }
+
+    std::string to_str() const
+    {
+        auto result = method_;
+        for (auto &param : params_)
+        {
+            result += ',' + param;
+        }
+        return result;
+    }
+
+    Type type() const { return type_; }
+    const std::string &method() const { return method_; }
+    const std::vector<std::string> &params() const { return params_; }
+    const std::string &operator[](std::size_t ind_of_param) const
+    { return params_[ind_of_param]; }
 
   private:
     Type type_;
     std::string method_;
-    std::string value_;
+    std::vector<std::string> params_;
 };
 } // namespace chord
 
