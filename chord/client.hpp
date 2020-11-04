b62f250fa14f42839bb0a0bb5299ff85853e7155
mu001999 <mu001999@outlook.com>
2020-11-04 16:35:54 +0800

    update

--- chord/client.hpp
+++ chord/client.hpp
@@ -1,6 +1,7 @@
 #ifndef __CHORD_CLIENT_HPP__
 #define __CHORD_CLIENT_HPP__
 
+#include <icarus/callbacks.hpp>
 #include <icarus/inetaddress.hpp>
 
 namespace chord
--- chord/server.hpp
+++ chord/server.hpp
@@ -6,6 +6,9 @@
 #include "instruction.hpp"
 #include "fingertable.hpp"
 
+#include <icarus/buffer.hpp>
+#include <icarus/eventloop.hpp>
+#include <icarus/tcpclient.hpp>
 #include <icarus/tcpserver.hpp>
 #include <icarus/tcpconnection.hpp>
 
@@ -64,11 +67,28 @@ class Server
         auto dst_port = static_cast<std::uint16_t>(std::stoi(value.substr(pos + 1)));
         auto src_port = listen_addr_.to_port();
 
-        auto server_addr = icarus::InetAddress(dst_ip.c_str(), dst_port);
+        /**
+         * wait here until things are ok or wrong
+        */
+        icarus::EventLoop loop;
+        icarus::InetAddress server_addr(dst_ip.c_str(), dst_port);
+        icarus::TcpClient client(&loop, server_addr, "chord client");
 
         /**
-         * then bind this server with one client
+         * set callbacks
         */
+        client.set_connection_callback([] (const icarus::TcpConnectionPtr &conn) {
+
+        });
+        client.set_message_callback([] (const icarus::TcpConnectionPtr &conn, icarus::Buffer *buf) {
+
+        });
+
+        /**
+         * loop until the process is finished
+        */
+        client.connect();
+        loop.loop();
     }
 
     void handle_instruction_get(const std::string &value)
