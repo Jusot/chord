5bbcfc11ab5936c4273176bc9e9ad20a33f1fde6
mu001999 <mu001999@outlook.com>
2020-11-06 11:26:01 +0800

    update node

--- chord/node.hpp
+++ chord/node.hpp
@@ -1,8 +1,13 @@
 #ifndef __CHORD_NODE_HPP__
 #define __CHORD_NODE_HPP__
 
+#include "message.hpp"
+
 #include <string>
+#include <thread>
 #include <functional>
+#include <icarus/eventloop.hpp>
+#include <icarus/tcpclient.hpp>
 #include <icarus/inetaddress.hpp>
 
 namespace chord
@@ -10,39 +15,20 @@ namespace chord
 class Node
 {
   public:
-    Node() : has_value_(false)
-    {
-        // ...
-    }
+    Node() = default;
 
     Node(const icarus::InetAddress &addr)
-      : has_value_(true)
-      , hash_value_(std::hash<std::string>{}(addr.to_ip_port()))
-      , addr_(addr)
-    {
-        // ...
-    }
-
-    Node(std::size_t hash_value,
-        const icarus::InetAddress &addr)
-      : has_value_(true)
-      , hash_value_(hash_value)
+      : hash_value_(std::hash<std::string>{}(addr.to_ip_port()))
       , addr_(addr)
     {
         // ...
     }
 
     Node(const Node &other)
-      : has_value_(other.has_value_)
-      , hash_value_(other.hash_value_)
+      : hash_value_(other.hash_value_)
       , addr_(other.addr_)
     {
         // ...
-    }
-
-    bool has_value() const
-    {
-        return has_value_;
     }
 
     std::size_t hash_value() const
@@ -55,8 +41,17 @@ class Node
         return addr_;
     }
 
+    bool operator==(const Node &rhs) const
+    {
+        return hash_value_ == rhs.hash_value_;
+    }
+
+    bool between(const Node &lnode, const Node &rnode) const
+    {
+
+    }
+
   private:
-    bool has_value_;
     /**
      * hash_value is calculated by the standard function hash simply
      *  instead of using sha-1 or sha-256
