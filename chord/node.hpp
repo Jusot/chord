97e73d99126b11deedad88d07664807b4dacd49c
mu001999 <mu001999@outlook.com>
2020-11-09 16:46:07 +0800

    use hashtype instead of direct size_t

--- chord/node.hpp
+++ chord/node.hpp
@@ -2,6 +2,7 @@
 #define __CHORD_NODE_HPP__
 
 #include "message.hpp"
+#include "hashtype.hpp"
 
 #include <string>
 #include <thread>
@@ -15,35 +16,23 @@ namespace chord
 class Node
 {
   public:
-    Node() : has_value_(false)
-    {
-        // ...
-    }
+    Node() = delete;
 
-    Node(const icarus::InetAddress &addr)
-      : has_value_(true)
-      , hash_value_(std::hash<std::string>{}(addr.to_ip_port()))
-      , addr_(addr)
+    explicit Node(const icarus::InetAddress &addr)
+      : hash_(addr), addr_(addr)
     {
         // ...
     }
 
     Node(const Node &other)
-      : has_value_(other.has_value_)
-      , hash_value_(other.hash_value_)
-      , addr_(other.addr_)
+      : hash_(other.hash_), addr_(other.addr_)
     {
         // ...
     }
 
-    bool has_value() const
+    const HashType &hash() const
     {
-        return has_value_;
-    }
-
-    std::size_t hash_value() const
-    {
-        return hash_value_;
+        return hash_;
     }
 
     const icarus::InetAddress &addr() const
@@ -53,46 +42,21 @@ class Node
 
     bool operator==(const Node &rhs) const
     {
-        return hash_value_ == rhs.hash_value_;
+        return hash_ == rhs.hash_;
+    }
+
+    bool between(const HashType &pre, const HashType &suc) const
+    {
+        return hash_.between(pre, suc);
     }
 
     bool between(const Node &pre, const Node &suc) const
     {
-        return between(pre.hash_value_, suc.hash_value_);
-    }
-
-    /**
-     * >.>.>.>.>.>.>.>.>.>.
-     * pre .<. this .<. suc
-     *
-     * 0        ||        0
-     * >.>.>.> this .>.>.>.
-     * suc .<.<.<.<.<.< pre
-    */
-    bool between(std::size_t pre, std::size_t suc) const
-    {
-        if (pre == suc)
-        {
-            return true;
-        }
-        else if (pre < suc)
-        {
-            return pre < hash_value_ && hash_value_ < suc;
-        }
-        else
-        {
-            return pre < hash_value_ || hash_value_ < suc;
-        }
+        return between(pre.hash_, suc.hash_);
     }
 
   private:
-    bool has_value_;
-    /**
-     * hash_value is calculated by the standard function hash simply
-     *  instead of using sha-1 or sha-256
-     *  and assume that its size is 64-bits
-    */
-    std::size_t hash_value_;
+    HashType hash_;
     icarus::InetAddress addr_;
 };
 } // namespace chord
